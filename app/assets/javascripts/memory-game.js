291721890548569c2a732a5f5d160e3e7e8207f2
Bri Dotson <bridotson@gmail.com>
2014-11-14 15:26:03 -0800

    add class of flipped to any card that has been flipped

--- app/assets/javascripts/memory-game.js
+++ app/assets/javascripts/memory-game.js
@@ -1,8 +1,13 @@
 $(function() {
+  // if two cards are flipped, check to see if they are the same
+  
   $(".card").click(stoutClickHandler);
+
 });
 
 function stoutClickHandler() {
   var theBeer = this.classList[1];
   $(this).attr("src","assets/memory-game/"+ theBeer + ".jpg");
+  // add the class of "flipped" to the card
+  $(this).addClass("flipped");
 }
