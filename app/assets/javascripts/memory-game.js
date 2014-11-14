e3a9725285bb2c130ace2b20fbb67575d879c0d4
Bri Dotson <bridotson@gmail.com>
2014-11-14 15:46:37 -0800

    write jquery to check if second card flipped has the same class name as the first

--- app/assets/javascripts/memory-game.js
+++ app/assets/javascripts/memory-game.js
@@ -1,6 +1,5 @@
 $(function() {
   // if two cards are flipped, check to see if they are the same
-  
   $(".card").click(stoutClickHandler);
 
 });
@@ -10,4 +9,14 @@ function stoutClickHandler() {
   $(this).attr("src","assets/memory-game/"+ theBeer + ".jpg");
   // add the class of "flipped" to the card
   $(this).addClass("flipped");
+
+  if ($(".flipped").length === 2) {
+    if ($(".flipped."+theBeer).length === 2) {
+      console.log("yay!");
+    } else {
+      console.log("boo!");
+    }
+    console.log("reset the class names here");
+    console.log("reset the images here");
+  }
 }
